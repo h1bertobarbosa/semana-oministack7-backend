fdf577953e1cbc1f0b3f2f4375e80627d991e726
humberto <humberto.obarbosa@gmail.com>
2019-07-18 20:43:46 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const path = require('path')
+const cors = require('cors')
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+mongoose.connect('mongodb+srv://semana:semana@cluster0-dtnyi.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true })
+app.use((req, res, next) => {
+  req.io = io
+
+  next()
+})
+app.use(cors())
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
+app.use(require('./routes'))
+server.listen(3333)
